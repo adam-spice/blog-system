68a6f2ca5bb64fdba5bc1b8fe1b728050c6f81e0
Adam Spice <adam@adam-spice/online>
2021-03-23 14:57:29 +1300

    finished auth

--- backend/settings.js
+++ backend/settings.js
@@ -2,9 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT;
-
 export const __PROD__ = process.env.NODE_ENV === 'production';
-
 export const CLIENT_URL = process.env.CLIENT_URL;
-
 export const MONGO_URI = process.env.MONGO_URI;
+export const JWT_SECRET = process.env.JWT_SECRET;
