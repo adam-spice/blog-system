6fd50bd95521c7aec2ac87424062f553a2e4ba4d
Adam Spice <adam@adam-spice/online>
2021-03-26 10:38:12 +1300

    need these

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: 'SEOBLOG',
+    API_DEVELOPMENT: 'http://localhost:3100/api',
+    API_PRODUCTION: 'http://clah.com/',
+    PRODUCTION: false,
+  },
+};
